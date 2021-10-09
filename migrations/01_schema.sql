6b66e10272d523443872faed98b83f2495c938d9
Lloyd Worth <lloydrww@gmail.com>
2021-10-09 00:24:21 +0000

    added more data, modified schema

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -3,62 +3,48 @@ DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
 
-CREATE TABLE "properties"(
-    "id" INTEGER NOT NULL,
-    "owner_id" INTEGER NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
-    "description" TEXT NOT NULL,
-    "thumbnail_photo_url" VARCHAR(255) NOT NULL,
-    "cover_photo_url" VARCHAR(255) NOT NULL,
-    "cost_per_night" INTEGER NOT NULL,
-    "parking_spaces" INTEGER NOT NULL,
-    "number_of_bathrooms" INTEGER NOT NULL,
-    "number_of_bedrooms" INTEGER NOT NULL,
-    "country" VARCHAR(255) NOT NULL,
-    "street" VARCHAR(255) NOT NULL,
-    "city" VARCHAR(255) NOT NULL,
-    "province" VARCHAR(255) NOT NULL,
-    "post_code" VARCHAR(255) NOT NULL,
-    "active" BOOLEAN NOT NULL
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
-ALTER TABLE
-    "properties" ADD PRIMARY KEY("id");
-CREATE TABLE "reservations"(
-    "id" INTEGER NOT NULL,
-    "start_date" DATE NOT NULL,
-    "end_date" DATE NOT NULL,
-    "property_id" INTEGER NOT NULL,
-    "guest_id" INTEGER NOT NULL
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
+
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
-ALTER TABLE
-    "reservations" ADD PRIMARY KEY("id");
-CREATE TABLE "property_reviews"(
-    "id" INTEGER NOT NULL,
-    "guest_id" INTEGER NOT NULL,
-    "property_id" INTEGER NOT NULL,
-    "reservation_id" INTEGER NOT NULL,
-    "rating" SMALLINT NOT NULL,
-    "message" TEXT NOT NULL
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
-ALTER TABLE
-    "property_reviews" ADD PRIMARY KEY("id");
-CREATE TABLE "users"(
-    "id" INTEGER NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL,
-    "password" VARCHAR(255) NOT NULL
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
 );
-ALTER TABLE
-    "users" ADD PRIMARY KEY("id");
-ALTER TABLE
-    "property_reviews" ADD CONSTRAINT "property_reviews_reservation_id_foreign" FOREIGN KEY("reservation_id") REFERENCES "reservations"("id");
-ALTER TABLE
-    "property_reviews" ADD CONSTRAINT "property_reviews_property_id_foreign" FOREIGN KEY("property_id") REFERENCES "properties"("id");
-ALTER TABLE
-    "reservations" ADD CONSTRAINT "reservations_property_id_foreign" FOREIGN KEY("property_id") REFERENCES "properties"("id");
-ALTER TABLE
-    "reservations" ADD CONSTRAINT "reservations_guest_id_foreign" FOREIGN KEY("guest_id") REFERENCES "users"("id");
-ALTER TABLE
-    "property_reviews" ADD CONSTRAINT "property_reviews_guest_id_foreign" FOREIGN KEY("guest_id") REFERENCES "users"("id");
-ALTER TABLE
-    "properties" ADD CONSTRAINT "properties_owner_id_foreign" FOREIGN KEY("owner_id") REFERENCES "users"("id");
